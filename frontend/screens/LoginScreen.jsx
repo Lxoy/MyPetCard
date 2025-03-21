71e0d0786c243c7979a61b7864e43a42b7bb2fff
Dean Vidovic <vidovicdean@gmail.com>
2025-03-21 22:37:27 +0100

    Login page
    Dodana slika i title

--- frontend/screens/LoginScreen.jsx
+++ frontend/screens/LoginScreen.jsx
@@ -1,11 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 
 export default function LoginScreen() {
   return (
-    <View className="bg-red-600 h-full w-full">
-      <Text>LoginScreen</Text>
+
+    // logo
+    <View className="bg-background flex justify-start items-center h-full">
+      <Image className="h-[350px] w-[350px] absolute" source={require('../img/logo-transparent.png')} />
+
+      {/* title */}
+      <View className='h-full w-full flex justify-around pt-40 pb-10'>
+        <View className='flex items-center'>
+          <Text className='font-bold tracking-wider text-5xl text-secondary'>Login</Text>
+        </View>
+      </View>
+    
+      {/* form */}
+      <View className='flex items-center mx-4 space-y-4'>
+        <View className='bg-black p-5 rounded-2xl w-full'></View>
+      </View>
+
     </View>
   )
 }
