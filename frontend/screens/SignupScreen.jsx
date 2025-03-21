29e3349efbf987888f063edd1773b31128dc859a
Dean Vidovic <vidovicdean@gmail.com>
2025-03-21 20:01:37 +0100

    Pocetni setup
    Ubacen NativeWind, React Navigation i React Native Reanimated

---
+++ frontend/screens/SignupScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+// tailwind
+import "../global.css";
+
+export default function SignupScreen() {
+  return (
+    <View>
+      <Text>SignupScreen</Text>
+    </View>
+  )
+}
