06d008f4a3b335671c086b3e6d528ae9b978065d
Lxoy <lo14loyt@gmail.com>
2025-04-17 00:39:03 +0200

    Added input check to account settings and started working on subscription screen

--- frontend/screens/appScreens/AccountSettingsScreen.jsx
+++ frontend/screens/appScreens/AccountSettingsScreen.jsx
@@ -1,17 +1,56 @@
 import React, { useState, useContext } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { AuthContext } from '../../context/AuthContext';
 import { StatusBar, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function AccountSettingsScreen({ navigation }) {
 
-  const { userData, logout } = useContext(AuthContext);
+  const { userData } = useContext(AuthContext);
 
   const [username, setUsername] = useState(userData.username);
   const [firstName, setFirstName] = useState(userData.first_name);
   const [lastName, setLastName] = useState(userData.last_name);
   const [phoneNumber, setPhoneNumber] = useState(userData.phone_number);
+
+  const [errorUsername, setErrorUsername] = useState("");
+  const [errorFirstName, setErrorFirstName] = useState("");
+  const [errorLastName, setErrorLastName] = useState("");
+  const [errorPhoneNumber, setErrorPhoneNumber] = useState("");
+
+  const handleUsernameChange = (value) => {
+    setUsername(value);
+    if (!value) {
+      setErrorUsername('Username is required');
+    } else if (value.length < 3) {
+      setErrorUsername('Username must be at least 3 characters');
+    } else {
+      setErrorUsername('');
+    }
+  };
+
+  const handleNameChange = (value, setValue, setError) => {
+    setValue(value);
+    if (!value) {
+      setError('');
+    } else if (!/^[A-Za-zčćžšđČĆŽŠĐ\s-]+$/.test(value)) {
+      setError('Only letters are allowed');
+    } else {
+      setError('');
+    }
+  };
+
+  const handlePhoneNumberChange = (value) => {
+    setPhoneNumber(value);
+    if (!value) {
+      setErrorPhoneNumber('');
+    } else if (!/^\+?[1-9]\d{7,14}$/.test(value)) {
+      setErrorPhoneNumber('Invalid phone number format');
+    } else {
+      setErrorPhoneNumber('');
+    }
+  };
+  
 
   return (
     <View className="flex-1 bg-secondary">
@@ -29,7 +68,7 @@ export default function AccountSettingsScreen({ navigation }) {
         <TouchableOpacity
           className="absolute top-4 right-4 px-3 py-1 rounded-full bg-white/70 flex-row items-center justify-center shadow-sm"
         >
-          <Text className="text-xl color-primary font-sfpro_medium">Save</Text>
+          <FontAwesomeIcon icon={faCheck} size={18} color="#4A90E2" />
         </TouchableOpacity>
 
         {/* Title */}
@@ -39,17 +78,48 @@ export default function AccountSettingsScreen({ navigation }) {
 
         {/* Fields */}
         <View className="mt-4">
-          <InputField label="Username" placeholder="Username" helper="Enter your username" value={username} size={30} />
-          <InputField label="First Name" placeholder="First Name" helper="Enter your first name" value={firstName} size={30}/>
-          <InputField label="Last Name" placeholder="Last Name" helper="Enter your last name" value={lastName} size={30} />
-          <InputField label="Phone Number" placeholder="+14155552671" value={phoneNumber} helper="Enter in format shown" />
+          <InputField
+            label="Username"
+            placeholder="Username"
+            helper="Enter your username"
+            value={username}
+            size={30}
+            action={handleUsernameChange}
+            error={errorUsername}
+          />
+
+          <InputField
+            label="First Name"
+            placeholder="First Name"
+            helper="Enter your first name"
+            value={firstName}
+            size={30}
+            action={(text) => handleNameChange(text, setFirstName, setErrorFirstName)}
+            error={errorFirstName} />
+
+          <InputField
+            label="Last Name"
+            placeholder="Last Name"
+            helper="Enter your last name"
+            value={lastName}
+            size={30}
+            action={(text) => handleNameChange(text, setLastName, setErrorLastName)}
+            error={errorLastName} />
+
+          <InputField 
+          label="Phone Number" 
+          placeholder="+385912345678" 
+          value={phoneNumber} 
+          helper="Enter in format shown" 
+          action={handlePhoneNumberChange} 
+          error={errorPhoneNumber} />
         </View>
       </ScrollView>
     </View>
   );
 }
 
-const InputField = ({ label, placeholder, value, helper, size }) => {
+const InputField = ({ label, placeholder, value, helper, size, action, error }) => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
@@ -59,19 +129,25 @@ const InputField = ({ label, placeholder, value, helper, size }) => {
 
       {/* Input */}
       <TextInput
-        className={`w-full bg-white rounded-xl px-4 py-2 text-base text-gray-900 shadow-sm border ${
-          isFocused ? 'border-primary' : 'border-lightgrey'
-        }`}
+        className={`w-full bg-white rounded-xl px-4 py-2 text-base text-gray-900 shadow-sm border ${error
+          ? 'border-error'
+          : isFocused
+            ? 'border-primary'
+            : 'border-lightgrey'
+          }`}
         placeholder={placeholder}
         value={value}
         onFocus={() => setIsFocused(true)}
         onBlur={() => setIsFocused(false)}
+        onChangeText={action}
+        maxLength={size}
       />
+
 
       {/* Helper + Character Count */}
       <View className="flex-row justify-between mt-1 px-1">
-        <Text className="text-xs text-darkgrey">{helper}</Text>
-        {label !== 'Phone Number' && <Text className="text-xs text-darkgrey">0/{size}</Text>}
+        {!error ? <Text className="text-xs text-darkgrey">{helper}</Text> : <Text className="text-xs text-error">{error}</Text>}
+        {label !== 'Phone Number' && <Text className="text-xs text-darkgrey">{value.length}/{size}</Text>}
       </View>
     </View>
   );
--- frontend/screens/appScreens/SubscriptionScreen.jsx
+++ frontend/screens/appScreens/SubscriptionScreen.jsx
@@ -1,27 +1,48 @@
-import React, {useState} from 'react';
-import { Button, FlatList, RefreshControl, ScrollView, SectionList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCircle } from '@fortawesome/free-regular-svg-icons';
+import { StatusBar, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 
 
-export default function SubscriptionScreen () {
-    return (
-        <View style={styles.body}>
-            <Text style={styles.text}>
-            Subscription Account
-            </Text>
+export default function SubscriptionScreen({ navigation }) {
+  return (
+    <View className="flex-1 bg-secondary">
+      <StatusBar barStyle="light-content" backgroundColor="black" />
+
+      {/* Back Button */}
+      <TouchableOpacity
+        className="absolute top-4 left-4 px-3 py-1 rounded-full bg-white/70 flex-row items-center justify-center shadow-sm"
+        onPress={() => navigation.goBack()}
+      >
+        <FontAwesomeIcon icon={faChevronLeft} size={18} color="#4A90E2" />
+      </TouchableOpacity>
+
+      {/* Title */}
+      <View className="mt-4 items-center">
+        <Text className="font-sfpro_regular text-2xl text-text">Subscription</Text>
+      </View>
+
+      <View className='flex-1 items-center justify-center'>
+        <View className="border-2 py-4 w-[80%] h-[270px] mb-4">
+          <Text className='font-sfpro_regular text-lg'>Standard</Text>
+          <Text className='font-sfpro_regular text-lg'>Features:</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 1</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 2</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 3</Text>
+
+        <FontAwesomeIcon className="absolute top-2 right-2 px-3 py-1" icon={faCircle} size={18} color="#4A90E2" />
         </View>
-      );
-}
+        <View className="border-2 py-4 w-[80%] h-[270px] mb-4">
+          <Text className='font-sfpro_regular text-lg'>Premium</Text>
+          <Text className='font-sfpro_regular text-lg'>Features:</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 1</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 2</Text>
+          <Text className='font-sfpro_regular text-lg'>- feature 3</Text>
+        </View>
+      </View>
 
-const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#fff'
-  },
-  text: {
-    color: '#000',
-    fontSize: 25,
-    fontStyle: 'italic',
-    margin: 15
-  },
-});
+
+    </View>
+  );
+}
